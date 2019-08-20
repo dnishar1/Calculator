170bc86d5c41a614fb13643eccad19d4c2aed14f
dnishar1 <dhairyatnishar@gmail.com>
2019-08-20 13:55:14 -0400

    Brand new start to the app

--- logic.js
+++ logic.js
@@ -1,16 +1,41 @@
-const button = document.getElementById('clear-button');
-
-window.onload = function(){
-    button.addEventListener('click', updateButton);
-}
-function updateButton() {
-    if (button.value === 'C') {
-      button.value = 'CE';
-    } else {
-      button.value = 'C';
-    }
-  }
 
 function display(value){
-  document.getElementById("answer").value += value
+  document.getElementById("answer").value += value;
+}
+
+function evaluate(){
+  let evaluating = document.getElementById("answer").value;
+  let equal = eval(evaluating);
+  document.getElementById("answer").value = equal;
+}
+
+function clearScreen(){
+  document.getElementById("answer").value = "";
+}
+
+function number(value){
+  
+}
+
+function decimal(){
+
+}
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+function equals(){
+
 }
