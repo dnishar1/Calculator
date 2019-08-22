8c2b7b52b32173484bb8009d3467a8ee44857d4e
Dhairya Nishar <dhairyatnishar@gmail.com>
2019-08-22 07:55:02 -0400

    Added structure to the JS code

--- logic.js
+++ logic.js
@@ -1,16 +1,84 @@
-const button = document.getElementById('clear-button');
-
-window.onload = function(){
-    button.addEventListener('click', updateButton);
+function evaluate(){
+  let evaluating = document.getElementById("answer").value;
+  let equal = eval(evaluating);
+  document.getElementById("answer").value = equal;
 }
-function updateButton() {
-    if (button.value === 'C') {
-      button.value = 'CE'; 
-    } else {
-      button.value = 'C';
-    }
-  }
+
+function clearScreen(){
+  document.getElementById("answer").value = "";
+}
+
+/* NEW FUNCTIONALITY */
+
+let operator1 = null;
+let operator2 = null;
+let operation = null;
 
 function display(value){
-  document.getElementById("answer").value += value
+  document.getElementById("answer").value = value;
+  //calculator.dispValue = dispValue === '0' ? value : dispValue + value;
+}
+
+function number(value) {
+  // if 'operator1' is equal to null
+  // then let's set 'operator1' to the 'value' parameter
+  // else if 'operator1' is not equal to null
+  // then let's set concatenate the 'value' parameter to 'operator1'
+  if (operator1 === null){
+    operator1 = value;
+    display(operator1);
+  }
+    else if (operator1 !== null){
+      operator1 = operator1.concat(value);
+      display(operator1);
+  }
+  
+}
+
+function decimal(dot) {
+//  if (diaplay(value).include(dot)){
+//    display(value) += dot;
+//  }
+}
+
+function add(operator1, operator2) {
+  if (operator1 != null && operator1 >= 0 ){
+    return operator1 += operator2
+  }
+  else if (operator2 != null && operator2 >= 0){
+    return operator2 += operator1
+  }
+}
+
+function subtract(operator1, operator2) {
+  if (operator1 != null && operator1 >= 0){
+    return operator1 -= operator2
+  }
+  else if (operator2 != null && operator2 >= 0){
+    return operator2 -= operator1
+  }
+}
+
+function multiply(operator1, operator2) {
+  if (operator1 != null && operator1 >= 0){
+    return operator1 *= operator2
+  }
+  else if (operator2 != null && operator2 >= 0){
+    return operator2 *= operator1
+  }
+}
+
+function divide(operator1, operator2) {
+  if (operator1 != null && operator1 >= 0){
+    return operator1 /= operator2
+  }
+  else if (operator2 != null && operator2 >= 0){
+    return operator2 /= operator1
+  }
+}
+
+function equals(operator1, operator2) {
+  //if (operator1 && operator2){
+    //value = operator1 + operator2
+  //}
 }
